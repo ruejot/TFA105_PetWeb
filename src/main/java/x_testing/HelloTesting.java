1a602360c7c6fbf6466c83b46f4cbe0be60ed755
LAPTOP-PATF7J21\Tibame_T14 <b860627@gmail.com>
2022-01-09 10:30:13 +0800

    done pull;push+1

--- src/main/java/x_testing/HelloTesting.java
+++ src/main/java/x_testing/HelloTesting.java
@@ -4,6 +4,7 @@ public class HelloTesting {
 //test add > commit > push
 	public static void main(String[] args) {
 		System.out.println("FIrst line.");
+		System.out.println("Second line.");
 //		You can play any GIT comments here!
 	}
 }
