9d94abaa2351becfee702438b57b7544a06e6f6b
RWei_C <cawic107@gmail.com>
2022-01-09 09:55:44 +0800

    Activation today.

--- src/main/java/x_testing/HelloTesting.java
+++ src/main/java/x_testing/HelloTesting.java
@@ -2,4 +2,8 @@ package x_testing;
 
 public class HelloTesting {
 //test add > commit > push
+	public static void main(String[] args) {
+		System.out.println("FIrst line.");
+//		You can play any GIT comments here!
+	}
 }
